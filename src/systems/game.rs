d5d608377779744b4dd01611dd64123e7e7aadc0
Wesley M. Oliveira <wesley@sortium>
2022-03-31 22:57:11 -0300

    feat: add spritesheet support

--- src/systems/game.rs
+++ src/systems/game.rs
@@ -1,13 +1,19 @@
-use crate::entities::InitialState;
+use crate::entities::{InitialState, Ufo};
+//use bevy::ecs::Query;
 use bevy::prelude::*;
 
-pub fn game(mut query: Query<&mut InitialState>, time: Res<Time>) {
-    let state = &mut query.iter_mut().next().unwrap();
+pub fn game(
+    mut state_query: Query<&mut InitialState>,
+    time: Res<Time>,
+    mut ufo_query: Query<&mut Ufo>,
+) {
+    /* let state = &mut query.iter_mut().next().unwrap();
 
     state.timer += time.delta_seconds();
 
     if !state.in_progress && state.timer > 5.0 {
         println!("Five seconds passed");
         state.timer = 0.0;
-    }
+    } */
+    println!("{}", time.delta_seconds() * 1000.0);
 }
--- src/systems/init.rs
+++ src/systems/init.rs
@@ -1,16 +1,19 @@
 use crate::entities::{InitialState, Ufo};
 use bevy::prelude::*;
 
-pub fn init(mut commands: Commands, asset_server: Res<AssetServer>) {
+pub fn init(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
     println!("initializing game with an sprite");
+    let texture_handle = asset_server.load("spritesheet.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(16.0, 16.0), 16, 16);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
 
     commands
         .spawn()
         .insert_bundle(OrthographicCameraBundle::new_2d())
-        .insert_bundle(SpriteBundle {
-            texture: asset_server.load("ufo.png"),
-            ..Default::default()
-        })
         .insert_bundle((
             InitialState {
                 in_progress: false,
@@ -18,4 +21,19 @@ pub fn init(mut commands: Commands, asset_server: Res<AssetServer>) {
             },
             Ufo { is_alive: true },
         ));
+
+    let l = 32;
+    for y in 0..l {
+        for x in 0..l {
+            commands.spawn_bundle(SpriteSheetBundle {
+                texture_atlas: texture_atlas_handle.clone(),
+                transform: Transform::from_translation(Vec3::new(
+                    (x as f32) * 16.0,
+                    (y as f32) * 16.0,
+                    0.0,
+                )),
+                ..Default::default()
+            });
+        }
+    }
 }
